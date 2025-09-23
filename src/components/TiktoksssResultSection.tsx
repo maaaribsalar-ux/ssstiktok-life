ef30fac60de89cc3df5483c80997bd145b264503
maaaribsalar-ux <maaaribsalar@gmail.com>
2025-09-23 22:50:59 +0500

    Update TiktoksssResultSection.tsx

--- src/components/TiktoksssResultSection.tsx
+++ src/components/TiktoksssResultSection.tsx
@@ -54,7 +54,7 @@ function ResultSection({ data, onReset }: ResultSectionProps) {
         <div class="bg-white rounded overflow-hidden backdrop-blur-sm border border-white/10 p-4">
           <div class="flex flex-col md:flex-row gap-4">
             <div class="md:w-1/3 flex-shrink-0">
-              <div class="relative rounded-lg overflow-hidden max-h-[430px]">
+              <div class="relative rounded overflow-hidden max-h-[430px]">
                 {getVideoUrl() && (
                   <video 
                     controls 
@@ -95,7 +95,7 @@ function ResultSection({ data, onReset }: ResultSectionProps) {
 
               <div class="space-y-2">
                 {data?.result?.videoSD && (
-                  <button class="download-button bg-fuchsia-600 hover:from-fuchsia-600 hover:to-fuchsia-900 w-full p-3 rounded-lg text-white flex items-center justify-center">
+                  <button class="download-button bg-fuchsia-600 hover:from-fuchsia-600 hover:to-fuchsia-900 w-full p-3 rounded text-white flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
                     </svg> 
@@ -106,7 +106,7 @@ function ResultSection({ data, onReset }: ResultSectionProps) {
                 )}
 
                 {(data?.result?.videoHD || data?.result?.video_hd) && (
-                  <button class="download-button bg-fuchsia-600 hover:from-fuchsia-600 hover:to-fuchsia-900 w-full p-3 rounded-lg text-white flex items-center justify-center">
+                  <button class="download-button bg-fuchsia-600 hover:from-fuchsia-600 hover:to-fuchsia-900 w-full p-3 rounded text-white flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
                     </svg> 
@@ -117,7 +117,7 @@ function ResultSection({ data, onReset }: ResultSectionProps) {
                 )}
 
                 {data?.result?.videoWatermark && (
-                  <button class="download-button bg-fuchsia-600 hover:from-fuchsia-600 hover:to-fuchsia-900 hover:to-green-300 w-full p-3 rounded-lg text-white flex items-center justify-center">
+                  <button class="download-button bg-fuchsia-600 hover:from-fuchsia-600 hover:to-fuchsia-900 hover:to-green-300 w-full p-3 rounded text-white flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"></path>
                     </svg> 
@@ -128,7 +128,7 @@ function ResultSection({ data, onReset }: ResultSectionProps) {
                 )}
 
                 {data?.result?.music && (
-                  <button class="download-button bg-fuchsia-600 hover:from-fuchsia-600 hover:to-fuchsia-900 hover:to-yellow-300 w-full p-3 rounded-lg text-white flex items-center justify-center">
+                  <button class="download-button bg-fuchsia-600 hover:from-fuchsia-600 hover:to-fuchsia-900 hover:to-yellow-300 w-full p-3 rounded text-white flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"></path>
                     </svg> 
@@ -140,7 +140,7 @@ function ResultSection({ data, onReset }: ResultSectionProps) {
 
                 <button 
                   onClick={onReset}
-                  class="download-button bg-fuchsia-600 hover:from-fuchsia-600 hover:to-fuchsia-900 w-full p-3 rounded-lg text-white flex items-center justify-center">
+                  class="download-button bg-fuchsia-600 hover:from-fuchsia-600 hover:to-fuchsia-900 w-full p-3 rounded text-white flex items-center justify-center">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path>
                   </svg>
@@ -156,4 +156,5 @@ function ResultSection({ data, onReset }: ResultSectionProps) {
 }
 
 export default ResultSection;
+
 
